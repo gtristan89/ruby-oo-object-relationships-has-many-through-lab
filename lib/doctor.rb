4ee98b73c91167f1352a9753161e88249d72c636
Tristan Gibbs <gtristan89@gmail.com>
2021-02-17 19:28:29 -0700

    commit tree 249544a7d6edfed82fe389e23a528f1eab932407

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,35 @@
 
+class Doctor
+    attr_accessor :name, :patient
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @appointments = []
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, patient)
+        appointment = Appointment.new(date, patient, self)
+        @appointments << appointment
+        appointment
+    end
+
+    def appointments
+        Appointment.all {|appointment| self.appointment = self}
+    end
+
+    def patients
+        @appointments.collect do |appointment|
+            appointment.patient
+        end
+    end
+end
